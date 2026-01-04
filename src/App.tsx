58c4a91e3731e933b69b16152b0a07206ec6bda1
Lucas Marte <lucasmarte@MacBook-Pro-de-Lucas.local>
2026-01-04 17:35:10 -0300

    fix: integrar FinanceProvider no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,24 @@ import Objetivos from './pages/Objetivos';
 import Cartoes from './pages/Cartoes';
 import Transacoes from './pages/Transacoes';
 import Perfil from './pages/Perfil';
+import { FinanceProvider } from './contexts/FinanceContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/objetivos" element={<Objetivos />} />
-          <Route path="/cartoes" element={<Cartoes />} />
-          <Route path="/transacoes" element={<Transacoes />} />
-          <Route path="/perfil" element={<Perfil />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <FinanceProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/objetivos" element={<Objetivos />} />
+            <Route path="/cartoes" element={<Cartoes />} />
+            <Route path="/transacoes" element={<Transacoes />} />
+            <Route path="/perfil" element={<Perfil />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </FinanceProvider>
   );
 }
 
 export default App;
-
